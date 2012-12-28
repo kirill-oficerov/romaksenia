b2fb30c3d90e0b6794737cc36fe6ae4bfc61e592
kirill oficerov <kirill.oficerov@pigsels.com>
2012-12-28 15:05:55 +0200

    events http://shoptogether.megaplan.ru/task/1000086/card/?fromList=1000086

--- Wd/widgets/Events.php
+++ Wd/widgets/Events.php
@@ -18,25 +18,25 @@ class Wd_Widgets_Events extends WP_Widget {
 		global $wpdb;
 
 		$query = "SELECT * FROM wp_terms
-LEFT JOIN wp_term_taxonomy ON wp_term_taxonomy.term_id = wp_terms.term_id
-LEFT JOIN wp_term_relationships ON wp_term_relationships.term_taxonomy_id = wp_term_taxonomy.term_taxonomy_id
-LEFT JOIN wp_posts ON wp_posts.ID = wp_term_relationships.object_id
+INNER JOIN wp_term_taxonomy ON wp_term_taxonomy.term_id = wp_terms.term_id
+INNER JOIN wp_term_relationships ON wp_term_relationships.term_taxonomy_id = wp_term_taxonomy.term_taxonomy_id
+INNER JOIN wp_posts ON wp_posts.ID = wp_term_relationships.object_id
 WHERE wp_terms.name = 'Ивенты'";
 
 		$terms = $wpdb->get_results($query);
 
 
-		if(!count($terms)) {
-			echo 'id of Ивенты not found';
-			return;
-		}
+//		if(!count($terms)) {
+//			echo 'id of Ивенты not found';
+//			return;
+//		}
 		?>
 
 	<div class="widget"><h3>Ивенты</h3><hr>
 		<div>
 			<?
 			foreach($terms as $key => $post) { ?>
-				<div class="page_item page-item-<?=$post->term_id?>"><a href="http://wedigital.dev/<?=$post->post_name ?>/" class="event-title"><?=$post->post_title?></a>
+				<div class="page_item page-item-<?=$post->term_id?>"><a href="<?=HTTP_HOST?>/<?=$post->post_name ?>/" class="event-title"><?=$post->post_title?></a>
 					<div class="event-content">
 						<?
 						$wordsAmount = Wd::get('settings')->getValue(Settings::MAX_EVENT_LENGTH_WORDS);
