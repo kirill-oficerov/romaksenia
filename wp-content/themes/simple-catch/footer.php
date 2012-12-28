84bf15cbcfb3f7ad054d9189d57ae6e03b839c5e
kirill oficerov <kirill.oficerov@pigsels.com>
2012-12-28 14:50:59 +0200

    yandex metrika

--- wp-content/themes/simple-catch/footer.php
+++ wp-content/themes/simple-catch/footer.php
@@ -46,9 +46,6 @@
 <?php wp_footer(); ?>
 <script type="text/javascript">
 	(function($) {
-
-
-
 		$(function() {
 			(function() {
 				var tk = document.createElement('script');
@@ -69,13 +66,54 @@
 
 
 
-
-
-
-
 	})(jQuery);
 
 
 </script>
+<!-- Yandex.Metrika counter
+-->
+<script
+	type="text/javascript">
+	(function (d, w, c) {
+		(w[c] = w[c] || []).push(function()
+		{
+			try {
+				w.yaCounter19068343 = new
+					Ya.Metrika({id:19068343,
+					webvisor:true,
+					clickmap:true,
+					trackLinks:true,
+					accurateTrackBounce:true,
+					trackHash:true});
+			} catch(e) { }
+		});
+
+		var n =
+			d.getElementsByTagName("script")[0],
+			s =
+				d.createElement("script"),
+			f = function () {
+				n['removed'].insertBefore(s, n); };
+		s.type =
+			"text/javascript";
+		s.async = true;
+		s.src = (d.location.protocol == "https:" ?
+			"https:" : "http:") +
+			"//mc.yandex.ru/metrika/watch.js";
+
+		if (w.opera == "[object Opera]")
+		{
+			d.addEventListener("DOMContentLoaded", f,
+				false);
+		} else { f(); }
+	})(document, window,
+		"yandex_metrika_callbacks");
+</script>
+<noscript><div><img
+	src="//mc.yandex.ru/watch/19068343" style="position:absolute;
+left:-9999px;" alt=""
+	/></div></noscript>
+<!-- /Yandex.Metrika counter
+-->
 </body>
 </html>
