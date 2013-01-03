05c8c6edba366c38314e5843543912a9191a73db
kirill oficerov <kirill.oficerov@pigsels.com>
2013-01-03 21:29:25 +0200

    yandex.metrika replaced

--- wp-content/themes/simple-catch/footer.php
+++ wp-content/themes/simple-catch/footer.php
@@ -85,50 +85,6 @@
 	}
 
 </script>
-<!-- Yandex.Metrika counter
--->
-<script
-	type="text/javascript">
-	(function (d, w, c) {
-		(w[c] = w[c] || []).push(function()
-		{
-			try {
-				w.yaCounter19068343 = new
-					Ya.Metrika({id:19068343,
-					webvisor:true,
-					clickmap:true,
-					trackLinks:true,
-					accurateTrackBounce:true,
-					trackHash:true});
-			} catch(e) { }
-		});
-
-		var n =
-			d.getElementsByTagName("script")[0],
-			s =
-				d.createElement("script"),
-			f = function () {
-				n['removed'].insertBefore(s, n); };
-		s.type =
-			"text/javascript";
-		s.async = true;
-		s.src = (d.location.protocol == "https:" ?
-			"https:" : "http:") +
-			"//mc.yandex.ru/metrika/watch.js";
-
-		if (w.opera == "[object Opera]")
-		{
-			d.addEventListener("DOMContentLoaded", f,
-				false);
-		} else { f(); }
-	})(document, window,
-		"yandex_metrika_callbacks");
-</script>
-<noscript><div><img
-	src="//mc.yandex.ru/watch/19068343" style="position:absolute;
-left:-9999px;" alt=""
-	/></div></noscript>
-<!-- /Yandex.Metrika counter
--->
+<!-- Yandex.Metrika counter --><script type="text/javascript">(function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter19155310 = new Ya.Metrika({id:19155310, webvisor:true, clickmap:true, trackLinks:true, accurateTrackBounce:true}); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks");</script><noscript><div><img src="//mc.yandex.ru/watch/19155310" style="position:absolute; left:-9999px;" alt="" /></div></noscript><!-- /Yandex.Metrika counter -->
 </body>
 </html>
