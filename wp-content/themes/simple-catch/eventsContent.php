07ab2c43c2ab89b6e671182a6f137d3ecd027667
kirill oficerov <kirill.oficerov@pigsels.com>
2013-01-11 22:51:20 +0200

    events http://shoptogether.megaplan.ru/task/1000114/card/?fromList=1000114

---
+++ wp-content/themes/simple-catch/eventsContent.php
@@ -0,0 +1,128 @@
+<?php
+/**
+ * This is the template that displays content for index and archive page
+ *
+ * @package Catch Themes
+ * @subpackage Simple_Catch
+ * @since Simple Catch 1.3.2
+ */
+global $wp_object_cache;
+get_header();
+?>
+<script type="text/javascript">
+	if($ == undefined) {
+		var $ = jQuery;
+	}
+	(function($) {
+		$(function() {
+			wdPrettyPhoto();
+		});
+	})(jQuery);
+</script>
+			<?php if ( have_posts() ) : 
+                while( have_posts() ):
+	                the_post(); ?>
+
+                        <?php
+	                    global $post;
+//	                    $tags = $wp_object_cache->get($post->ID, 'category_relationships');
+//	                    $isEvent = false;
+//	                    foreach($tags as $key=>$tag) {
+//		                    if($tag->name == 'Ивенты') {
+//			                    $isEvent = true;
+//			                    break;
+//		                    }
+//	                    }
+//	                    if($isEvent) {
+//		                    continue;
+//	                    }
+	                ?>
+                <div <?php post_class(); ?> >
+	                    <div class="col8" style="min-width: 100%">
+                            <div style="float: left; margin-right: -150px; min-width: 100%; ">
+			                    <h2 class="entry-title" style="margin: 10px 150px 3px 0px; ">
+	                                <a href="<?php the_permalink() ?>" title="" rel="bookmark" ><?php the_title(); ?></a>
+	                            </h2>
+                            </div>
+<!--	                --><?//
+//                    // tags
+//					$tags = $wp_object_cache->get($post->ID, 'category_relationships');
+//                    $output = '<ul class="category-list tags">';
+//                    foreach($tags as $key => $tag) {
+//	                    if(!is_null($tag->category_settings)) {
+//							$categorySettings = unserialize($tag->category_settings);
+//	                    }
+//	                    $output .= '<li ' . (isset($categorySettings['class']) ? 'class="' . $categorySettings['class'] . '"' : '') . ' ><a  href="' . HTTP_HOST . '/category/' . $tag->slug . '">' . $tag->name . '</a></li>';
+//	                    unset($categorySettings);
+//                    }
+//                    $output .= '</ul>';
+//	                echo $output;
+//	                ?>
+
+
+	                    <?php $excerpt = the_excerpt(false);
+//		                    $contentBegin = strrpos($excerpt, '<p style="" rel="begin-of-the-excerpt-text">');
+//		                    $content = substr($excerpt, $contentBegin);
+		                    $content = substr($excerpt, 3);
+		                    $content = substr($content, 0, -5);
+		                    echo '<script type="text/javascript" src="//yandex.st/share/share.js" charset="utf-8"></script>
+				<div class="yashare-auto-init" data-yashareL10n="ru" data-yashareType="none"
+				data-yashareQuickServices="vkontakte,facebook,twitter,odnoklassniki,lj,gplus,pinterest"></div>'
+		                    ?>
+		                    <div class="clear" style="height: 1px; width: 1px; "></div>
+
+		                    <div style="text-align: right; float: right; margin: 0px 0px 0px 15px;">
+			                    <?
+			                        $picture = get_the_post_thumbnail( null, 'featured', '' );
+				                    if(!empty($picture)) {
+					                    $pictureSrc = array();
+					                    preg_match('/src="[^"]+"/', $picture, $pictureSrc);
+					                    $settings = unserialize($post->settings);
+					                    if($settings) {
+						                    $width = $settings['width'];
+						                    $height = $settings['height'];
+						                    $picture = str_replace('<img', '<img style="width:' . $width . 'px; height:' . $height . 'px;"', $picture);
+					                    }
+					                    ?>
+                                    <a rel="prettyPhoto" href=<?=substr($pictureSrc[0], 4)?> title="<?php the_title_attribute( 'echo=0' ) ?>">
+<!--				                    --><?php // the_post_thumbnail( 'featured' ); ?>
+				                    <?php echo $picture ?>
+			                    </a>
+			                        <? } ?>
+		                    </div>
+		                    <? echo $content ?>
+	                    </div>
+                        <div class="row-end"></div>
+                    </div><!-- .post -->
+                    <hr />
+                    
+          			<?php endwhile;
+                    
+            		// Checking WP Page Numbers plugin exist
+					if ( function_exists('wp_pagenavi' ) ) : 
+						wp_pagenavi();
+					
+					// Checking WP-PageNaviplugin exist
+					elseif ( function_exists('wp_page_numbers' ) ) : 
+						wp_page_numbers();
+						   
+					else: 
+						global $wp_query;
+						if ( $wp_query->max_num_pages > 1 ) : 
+					?>
+							<ul class="default-wp-page">
+								<li class="previous"><? next_posts_link( 'Предыдущие' );?></li>
+								<li class="next"><? previous_posts_link('Следующие'); ?></li>
+								<div class="clear" style="margin-bottom: 10px;"></div>
+							</ul>
+                        <?php endif;
+ 					endif; 
+                    ?>
+
+                    			
+			<?php else : ?>
+                    <div class="post">
+                        <h2>К сожалению, в этой категории ещё нет статей. Но будут обязательно!</h2>
+                    </div><!-- .post -->
+			
+			<?php endif; ?>
---
+++ wp-content/themes/simple-catch/pageEvents.php
@@ -0,0 +1,49 @@
+<?php /* Template Name: Events */
+/**
+ * The main template file.
+ *
+ * This is the most generic template file in a Simple Catch theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @package Catch Themes
+ * @subpackage Simple_Catch
+ * @since Simple Catch 1.0
+ */
+
+
+
+get_header();
+
+if( function_exists( 'simplecatch_display_div' ) ) {
+	$themeoption_layout = simplecatch_display_div();
+}
+
+
+
+
+//set_query_var('pagename', '');
+//set_query_var('name', '');
+//wp_reset_query();
+//function gloss_remove_glossary_cat($wp_query) {
+//	set_query_var('pagename', 'кейсы');
+$query = array('category_name' => 'events');
+$paged = get_query_var('paged');
+if($paged) {
+	$query['paged'] = $paged;
+}
+query_posts($query);
+//query_posts('category_name=events');
+//}
+//add_action('pre_get_posts', 'gloss_remove_glossary_cat' );
+
+get_template_part('eventsContent'); ?>
+</div> <!-- keep content -->
+</div><!-- #content -->
+
+
+
+</div><!-- #main -->
+
+<?php get_footer(); ?>
