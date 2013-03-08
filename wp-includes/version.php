aefaad5d04d06d010c423ac6bd75885597e2ab15
kirill oficerov <kirill.oficerov@pigsels.com>
2013-03-08 14:02:53 +0200

    main page post width ok

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -6,7 +6,7 @@
  */
 // @todo kirill version
 //$wp_version = '3.4.2';
-$wp_version = '1.007';
+$wp_version = '1.008';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
