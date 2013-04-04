55e0922f1c9bd4b53b296530b9bae4680139547d
kirill oficerov <kirill.oficerov@pigsels.com>
2013-04-04 23:16:01 +0300

    events stick out

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -6,7 +6,7 @@
  */
 // @todo kirill version
 //$wp_version = '3.4.2';
-$wp_version = '1.009';
+$wp_version = '1.010';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
