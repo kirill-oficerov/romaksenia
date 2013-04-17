80083519a54035f4fabccfcffa21783ab933d757
kirill oficerov <kirill.oficerov@pigsels.com>
2013-04-17 21:17:10 +0300

    Wd_Parts_Images::CreateImageByPath copy file fix

--- Wd/parts/Images.php
+++ Wd/parts/Images.php
@@ -16,19 +16,20 @@ class Wd_Parts_Images {
 	/**
 	 * @param $imagePath @string server patch of original image
 	 * @param $sizeName @const Wd_Parts_Images::SIZE_*
-	 * @param $dimensions @array|null array('width', 'height')
+	 * @param $dimensions @array|null array('width', 'height'). if null - origin size of an image will be fetched
 	 * @param $token string|int 01339324934
 	 * @return mixed
 	 */
 	public static function CreateImageByPath($imagePath, $sizeName, $dimensions, $token) {
 		$originImage = new Imagick();
 		$originImage->readimage($imagePath);
+		$originImageSize = $originImage->getimagesize();
 		if(is_null($dimensions)) {
 			$dimensions['height'] = $originImage->getimageheight();
 			$dimensions['width'] = $originImage->getimagewidth();
  		}
-		$newImageNameParts = explode('.', $imagePath);
-		$originExtension = array_pop($newImageNameParts);
+		$newImagePathParts = explode('.', $imagePath);
+		$originExtension = array_pop($newImagePathParts);
 		if($originExtension == 'png') {
 			$originImage->resizeimage($dimensions['width'], $dimensions['height'], imagick::FILTER_UNDEFINED, 1);
 		} else {
@@ -36,13 +37,20 @@ class Wd_Parts_Images {
 			$originImage->setimagecompression(imagick::COMPRESSION_JPEG);
 			$originImage->setimagecompressionquality(80);
 		}
-		$newImageName = implode('.', $newImageNameParts) . '_' . Wd_Parts_Images::$_sizes[$sizeName] . '_' . $token . '.' . $originExtension;
+		$newImagePath = implode('.', $newImagePathParts) . '_' . Wd_Parts_Images::$_sizes[$sizeName] . '_' . $token . '.' . $originExtension;
 		$originImage->stripImage();
-		$originImage->writeImage($newImageName);
+		$originImage->writeImage($newImagePath);
+		// check if a new file is bigger than old file. In the case replace a new file with the old one
+		$newImage = new Imagick();
+		$newImage->readimage($newImagePath);
+		if($newImage->getimagesize() > $originImageSize) {
+			copy($imagePath, $newImagePath);
+		}
 		$originImage->clear();
+		$newImage->clear();
 		// make a relative path
 		$matches = array();
-		preg_match('/wp-content.uploads(.*)$/', $newImageName, $matches);
+		preg_match('/wp-content.uploads(.*)$/', $newImagePath, $matches);
 		return $matches[1];
 	}
 }
