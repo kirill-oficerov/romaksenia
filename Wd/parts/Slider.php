386f5794fa6adfb3cf7ea61f0a63f49594e29956
kirill oficerov <kirill.oficerov@pigsels.com>
2013-06-07 15:29:32 +0300

    tmp

--- Wd/parts/Slider.php
+++ Wd/parts/Slider.php
@@ -23,14 +23,17 @@ class Wd_Parts_Slider {
 			});
 			$firstImage = $terms[0];
 			$secondImage = $terms[1];
-			echo '
-			<div style="">
-				<a href="#"><img style="width: 750px; height: 310px;" src="' . $firstImage->image_guid . '"></a>
+			?>
+			<div style="position: relative; float: left; z-index: 1;">
+				<a href="#"><img style="width: 750px; height: 310px;" src="<?=$firstImage->image_guid?>"></a>
 			</div>
-			<div style="">
-				<a href="#"><img style="width: 750px; height: 310px;" src="' . $secondImage->image_guid . '"></a>
+			<div style="position: relative; float: left: z-index: 2;">
+				<div style="float: left; line-height: 100%; text-align: center; height: 100%; width: 51px;"><div style="background:url("images/icons.png") no-repeat -48px -19px; width: 51px; height: 51px;"></div></div>
+				<div style="float: left; margin: 0 0 0 5px;">
+					<a href="#"><img style="width: 750px; height: 310px;" src="<?=$secondImage->image_guid?>"></a>
+				</div>
 			</div>
-			';
+			<?
 
 		}
 	}
